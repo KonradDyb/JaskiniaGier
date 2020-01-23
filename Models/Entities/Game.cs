647578f3b80f896828387a3189b9af9e26d9a1cf
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-01-23 11:57:59 +0100

    Create first entities
    I create first entities in project. Next I will use them in mock repositories.

---
+++ Models/Entities/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaskiniaGier.Models.Entities
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Title { get; set; }
+        public string GameDescription { get; set; }
+        public string ImagePath { get; set; }
+        public decimal Price { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
---
+++ Models/Entities/SubGenre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaskiniaGier.Models.Entities
+{
+    public class SubGenre
+    {
+        public int SubGenreId { get; set; }
+        public string SubGenreName { get; set; }
+    }
+}
