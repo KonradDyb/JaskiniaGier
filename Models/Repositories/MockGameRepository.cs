726cfdee2d5cae1b56d72bf5b7f952c05cc39fd6
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-01-29 16:37:34 +0100

    Change view of product cards

--- Models/Repositories/MockGameRepository.cs
+++ Models/Repositories/MockGameRepository.cs
@@ -25,7 +25,7 @@ namespace JaskiniaGier.Models.Repositories
                     Title = "Wiedźmin 1",
                     GameDescription = "Nazywasz się Geralt z Rivii i jesteś wiedźminem, czyli za pieniądze zabijasz potwory. Przeszedłeś specjalne szkolenie, aby być najlepszym w swoim fachu, a twoje ciało zostało wzmocnione silnymi eliksirami. Masz amnezję i nie pamiętasz swojej przeszłości. Kaer Morhen, ostatnia twierdza wiedźminów, zostało zaatakowane przez tajemniczą organizację, gdy zaczynałeś leczyć tam swoje rany. Wygraliście bitwę, lecz straciliście sekretny przepis na mutagen - substancję potrzebną do tworzenia wiedźminów. Ci z twoich braci, którzy ocaleli, wyruszają, aby go odnaleźć i ukarać winnych. Pamiętasz czy nie, jesteś teraz jednym z nich.",
                     ImagePath = "wiedzmin.jpg",
-                    Price = "30,00 ZŁ",
+                    Price = "30,00 zł",
                     SubGenre = _subGenreRepository.SubGenres.ToList()[0]
                 },
                 new Game
@@ -34,7 +34,7 @@ namespace JaskiniaGier.Models.Repositories
                     Title = "Anno 1800",
                     GameDescription = "Strategia ekonomiczna od Ubisoftu",
                     ImagePath = "anno.jpg",
-                    Price = "100,00 ZŁ",
+                    Price = "100,00 zł",
                     SubGenre = _subGenreRepository.SubGenres.ToList()[5]
                 },
                  new Game
@@ -43,7 +43,7 @@ namespace JaskiniaGier.Models.Repositories
                     Title = "Gothic",
                     GameDescription = "RPG od Piranha Bytes",
                     ImagePath = "gothic.jpg",
-                    Price = "10,00 ZŁ",
+                    Price = "10,00 zł",
                     SubGenre = _subGenreRepository.SubGenres.ToList()[0]
                 },
                  new Game
@@ -52,7 +52,7 @@ namespace JaskiniaGier.Models.Repositories
                     Title = "Diablo",
                     GameDescription = "Hack&Slash od Blizzarda",
                     ImagePath = "diablo.jpg",
-                    Price = "30,00 ZŁ",
+                    Price = "30,00 zł",
                     SubGenre = _subGenreRepository.SubGenres.ToList()[1]
                 },
                  new Game
@@ -61,16 +61,16 @@ namespace JaskiniaGier.Models.Repositories
                     Title = "Commandos",
                     GameDescription = "Strategia taktyczna od Pyro Studios",
                     ImagePath = "commandos.jpg",
-                    Price = "10,00 ZŁ",
+                    Price = "10,00 zł",
                     SubGenre = _subGenreRepository.SubGenres.ToList()[4]
                 },
                  new Game
                 {
                     GameId = 6,
-                    Title = "RimWorld",
-                    GameDescription = "Strategia RTS od Ludeon Studios",
-                    ImagePath = "rimworld.jpg",
-                    Price = "130,00 ZŁ",
+                    Title = "Frostpunk",
+                    GameDescription = "Strategia RTS od 11 Bit Studios",
+                    ImagePath = "frostpunk.jpg",
+                    Price = "44,00 zł",
                     SubGenre = _subGenreRepository.SubGenres.ToList()[3]
                 },
                   new Game
@@ -79,8 +79,17 @@ namespace JaskiniaGier.Models.Repositories
                     Title = "World Of Warcraft",
                     GameDescription = "Rozszerzenie do WoWa",
                     ImagePath = "wow.jpg",
-                    Price = "170,00 ZŁ",
+                    Price = "170,00 zł",
                     SubGenre = _subGenreRepository.SubGenres.ToList()[2]
+                },
+                   new Game
+                {
+                    GameId = 8,
+                    Title = "Sid Meier's Civilization VI",
+                    GameDescription = "Strategia Ekonomiczna od Firaxis Games",
+                    ImagePath = "civ.jpg",
+                    Price = "44,00 zł",
+                    SubGenre = _subGenreRepository.SubGenres.ToList()[5]
                 }
             };
 
