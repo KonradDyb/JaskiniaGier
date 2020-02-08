4d782b8887340d8b8464f246c8c1b1fe30c0e622
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-02-08 23:31:02 +0100

    Add extension method to IEnumerable<T>

---
+++ Models/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaskiniaGier.Models
+{
+    public static class Extension
+    {
+        public static bool IsAny<T>(this IEnumerable<T> data)
+        {
+            return data != null && data.Any();
+        }
+    }
+}
