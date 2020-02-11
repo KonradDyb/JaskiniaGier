955b4c8b03841c770da30cc5245054faf3504a01
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-02-11 11:59:35 +0100

    Create cart items counter

---
+++ Components/CartSummary.cs
@@ -0,0 +1,35 @@
+﻿using JaskiniaGier.Models;
+using JaskiniaGier.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaskiniaGier.Components
+{
+    public class CartSummary : ViewComponent
+    {
+        private readonly Cart _cart;
+
+        public CartSummary(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _cart.GetCartItems();
+            _cart.CartItems = items;
+
+            var cartViewModel = new CartViewModel
+            {
+                Cart = _cart,
+                CartTotal = _cart.GetCartTotal()
+            };
+
+            return View(cartViewModel);
+        }
+    }
+}
