f1fa5e5647eadd411551a9c9dc61dc4508ced12a
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-02-11 10:11:48 +0100

    Create OrderDTO class

---
+++ Models/OrderDTO.cs
@@ -0,0 +1,16 @@
+﻿using JaskiniaGier.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaskiniaGier.Models
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public string OrderPlaced { get; set; }
+        public decimal Total { get; set; }
+    }
+}
