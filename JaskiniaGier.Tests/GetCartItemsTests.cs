ce450cbdd28b55675e5167295411bc9ef777b6af
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-02-28 18:23:56 +0100

    Fix tests project

---
+++ JaskiniaGier.Tests/GetCartItemsTests.cs
@@ -0,0 +1,54 @@
+using JaskiniaGier.Models;
+using JaskiniaGier.Models.Repositories.MockRepositories;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JaskiniaGier.Tests
+{
+    public class GetCartItemsTests : TestBase
+    {
+        [Fact]
+        public async Task ShouldReturn2CartItems()
+        {
+            //Arrange
+            UseSqlServer();
+            using var context = GetDbContext();
+            var cart = new Mock<Cart>();
+            var cartItemsRepo = new CartItemsRepository();
+            cart.Object.CartItems = cartItemsRepo.CartItems.ToList();
+
+            //Act
+            var result = await cart.Object.GetCartItemsAsync();
+
+            //Assert
+            Assert.NotEmpty(result);
+            Assert.Equal(2, result.Count());
+            Assert.Contains(result, r => r.Game.Title == "Test2");
+            Assert.DoesNotContain(result, r => r.Game.Title == "Test60");
+        }
+
+
+
+       [Fact]
+        public async Task CheckIfCartIsEmpty()
+        {
+            //Arrange
+            UseSqlServer();
+            using var context = GetDbContext();
+            var cart = new Mock<Cart>();
+            cart.Object.CartItems = new List<CartItem>();
+
+            //Act
+            var result = await cart.Object.GetCartItemsAsync();
+
+            //Assert
+            Assert.Empty(result);
+
+        }
+
+       
+    }
+}
